1122032df5bee204d7ab823c61c3df159235cb50
Vaibhav Rawat <rawatvaibhav27@gmail.com>
2023-07-08 12:51:36 +0530

    Update

--- app/src/main/java/com/vaibhavrawat/myfirstproject/MainActivity.kt
+++ app/src/main/java/com/vaibhavrawat/myfirstproject/MainActivity.kt
@@ -7,9 +7,11 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
+import androidx.core.view.accessibility.AccessibilityEventCompat.setAction
+import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
-    var btnNext: Button?= null
+    var btnNext: Button ?= null
     var btnValidate : Button?= null
     var etName : EditText?= null
     var etInfo : EditText?= null
@@ -50,10 +52,29 @@ class MainActivity : AppCompatActivity() {
             }
         }
         btnNext?.setOnClickListener {
+            btnNext?.let {
+                Snackbar.make(it, "UNDO/DELETE THE VALUES" , Snackbar.LENGTH_INDEFINITE)
+                    .setAction("Clear"){
+                        Toast.makeText(this, "DATA CLEARED SUCCESSFULLY", Toast.LENGTH_SHORT).show()
+                        //var intent = Intent(this, ::class.java)
+                        //startActivity(intent)
+                        etName?.setText("")
+                       etInfo?.setText("")
+                       etMarks?.setText("")
+                        etPercentage?.setText("")
+                        etNumber?.setText("")
+
+                    }
+                   // .setAnchorView(btnNext)
+                    .show()
+
+            }
            // var intent = Intent(this, CheckboxRadioActivity::class.java)
-           // startActivity(intent)
-            finish()
+            //           // startActivity(intent)
+            //        // finish()
         }
     }
 
 }
+
+
